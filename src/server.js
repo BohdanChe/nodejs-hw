0ccd1ea623b7371926cadc979ff1932fa85fe27c
BohdanChe <bohdancheshenko@gmail.com>
2026-03-22 14:06:07 +0100

    Add logger middleware and wire it in server

--- src/server.js
+++ src/server.js
@@ -1,8 +1,8 @@
 import express from 'express';
 import cors from 'cors';    
 import 'dotenv/config';
-import pino from 'pino-http';
 import {connectMongoDB} from './db/connectMongoDB.js'; 
+import { logger } from './middleware/logger.js';
 import { notFoundHandler } from './middleware/notFoundHandler.js';  
 import { errorHandler } from './middleware/errorHandler.js';
 import notesRoutes from './routes/notesRoutes.js';
@@ -14,21 +14,7 @@ const PORT = process.env.PORT ?? 3000;
 
 app.use(express.json());
 app.use(cors()); 
-app.use(
-  pino({
-    level: 'info',
-    transport: {
-      target: 'pino-pretty',
-      options: {
-        colorize: true,
-        translateTime: 'HH:MM:ss',
-        ignore: 'pid,hostname',
-        messageFormat: '{req.method} {req.url} {res.statusCode} - {responseTime}ms',
-        hideObject: true,
-      },
-    },
-  }),
-);
+app.use(logger);
 
 
 app.use(notesRoutes);
