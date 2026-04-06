ad88b00de7dad7290cac3e6ac6a72ae3aed8f39e
BohdanChe <bohdancheshenko@gmail.com>
2026-04-06 17:18:35 +0200

    Normalize auth routes imports with refresh endpoint

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,8 +1,7 @@
 import { Router } from "express";
 import { celebrate } from "celebrate";
 import { registerUserSchema, loginUserSchema } from "../validations/authValidation.js";
-import { registerUser, loginUser,logoutUser } from "../controllers/authController.js";
-import { refreshUserSession } from "../controllers/authController.js";
+import { registerUser, loginUser, logoutUser, refreshUserSession } from "../controllers/authController.js";
 
 const router = Router();
 router.post("/auth/register", celebrate(registerUserSchema),
