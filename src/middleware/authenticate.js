5bc4999908b0e07a929c6fb0f0d09df9d6c76474
BohdanChe <bohdancheshenko@gmail.com>
2026-04-06 17:29:30 +0200

    Return 401 without message when user is missing

--- src/middleware/authenticate.js
+++ src/middleware/authenticate.js
@@ -27,7 +27,7 @@ export const authenticate = async (req, res, next) => {
 
     const user = await User.findById(session.userId);
     if (!user) {
-        throw createHttpError(401, 'No user found');
+        throw createHttpError(401);
     }
 
     req.user = user;
